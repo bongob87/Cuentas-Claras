e7c11ab4fc8fda44db19e823370dbb12af7325a9
bongob87 <barrantes.christhian@gmail.com>
2026-01-21 05:47:36 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b',
+        },
+        dark: {
+          bg: '#0f172a',
+          card: '#1e293b',
+          text: '#f1f5f9',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
